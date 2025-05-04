1a6bd596bc018ff96e8114c81104ee8a91673723
James Elgar <jnelgar@amazon.com>
2025-05-04 12:40:15 +0100

    Trying out surrealdb

--- src/dao/mod.rs
+++ src/dao/mod.rs
@@ -1,64 +1,172 @@
-use aws_sdk_dynamodb::operation::transact_write_items;
-use aws_sdk_dynamodb::Client;
-use aws_sdk_dynamodb::types::{AttributeValue, KeySchemaElement};
+use id::{SurrealId, Table};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use surrealdb::{RecordId, Surreal, engine::remote::ws::Client};
+use thiserror::Error;
 use uuid::Uuid;
 
-const TABLE_NAME: &'static str = "AgonTable";
+mod id;
 
+const USER_TAG: &'static str = "user";
+const TEAM_TAG: &'static str = "team";
+
+#[derive(Clone, Debug)]
+struct UserTable;
+impl Table for UserTable {
+    fn table_name() -> &'static str {
+        return USER_TAG;
+    }
+}
+
+#[derive(Clone, Debug)]
+struct TeamTable;
+impl Table for TeamTable {
+    fn table_name() -> &'static str {
+        return TEAM_TAG;
+    }
+}
+
+#[derive(Error, Debug)]
 pub enum DaoError {
-    InternalServerError,
+    #[error("internal error")]
+    InternalServerError(String),
 }
 
-struct Team {
-    id: String,
+pub struct Team {
+    pub id: String,
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct TeamContent {
+    #[serde(skip_serializing)]
+    id: SurrealId<TeamTable>,
     name: String,
+    created_by_user_id: SurrealId<UserTable>,
 }
 
-struct User {
-    id: String,
-    email: String,
-    first_name: String,
-    last_name: String,
+pub struct User {
+    pub id: String,
+    pub email: String,
+    pub first_name: String,
+    pub last_name: String,
+}
+
+impl From<surrealdb::Error> for DaoError {
+    fn from(error: surrealdb::Error) -> Self {
+        eprintln!("{error}");
+        DaoError::InternalServerError(error.to_string())
+    }
 }
 
 // PK=TEAM#team_id SK=TEAM
 // Primary index -> list team members - GSI1 -> List teams user is a member of
 // PK=TEAM#team_id SK=USER#user_id GSI1_PK=USER#user_id GSI1_SK=TEAM#team_id
 
-pub async fn create_team(
-    name: String,
-    client: &Client,
-) -> Result<Team, DaoError> {
-    let team = Team {
-        id: Uuid::new_v4().to_string(),
-        name,
-    };
-
-    let request = client
-        .put_item()
-        .table_name(TABLE_NAME)
-        .item("PK", AttributeValue::S(format!("TEAM#{}", team.id)))
-        .item("SK", AttributeValue::S("TEAMS".to_string()))
-        .item("id", AttributeValue::S(team.id.clone()))
-        .item("name", AttributeValue::S(team.name.clone()));
-
-    let request = transact_write_items()
-
-    request.send().await.map_err(|_| DaoError::InternalServerError)?;
-
-    Ok(team)
+#[derive(Clone)]
+pub struct Dao {
+    client: Surreal<Client>,
 }
 
-fn list_team_members(
-    team_id: String,
-    client: &Client,
-) -> Vec<User> {
-    let request = client
-        .query()
-        .table_name(TABLE_NAME)
-        .key_condition_expression("#pk = :pk")
-        .expression_attribute_names("#pk", "PK")
-        .expression_attribute_values(":pk", );
+#[derive(Deserialize)]
+struct UserTeamsListResponse {
+    #[serde(rename = "->member->team")]
+    teams: Vec<TeamContent>,
+}
 
-    todo!()
+impl Dao {
+    pub fn create(client: Surreal<Client>) -> Self {
+        Self { client }
+    }
+
+    pub async fn create_team(
+        &self,
+        name: String,
+        created_by_user_id: String,
+    ) -> Result<Team, DaoError> {
+        let team = Team {
+            id: Uuid::new_v4().to_string(),
+            name,
+        };
+
+        println!("Creating team id={}", team.id);
+
+        let team_content = TeamContent {
+            id: SurrealId::new(&team.id.clone()),
+            name: team.name.clone(),
+            created_by_user_id: SurrealId::new(&created_by_user_id),
+        };
+
+        let team_content_json = serde_json::to_value(&team_content).map_err(|err| {
+            // TODO Log error
+            DaoError::InternalServerError("Failed to serialize team content".to_string())
+        })?;
+
+        let query = r#"
+            BEGIN TRANSACTION;
+            CREATE $team_id CONTENT $team_data;
+            RELATE $user_id -> member -> $team_id CONTENT { joined_at: time::now() };
+            COMMIT TRANSACTION;
+        "#;
+
+        let response = self
+            .client
+            .query(query)
+            .bind(("team_id", RecordId::from((TEAM_TAG, &team.id))))
+            .bind(("user_id", RecordId::from((USER_TAG, &created_by_user_id))))
+            .bind(("team_data", team_content_json))
+            .await?;
+
+        match response.check() {
+            Err(err) => println!("Error in the query {}", err),
+            Ok(_) => println!("Query all good"),
+        }
+
+        Ok(team)
+    }
+
+    pub async fn list_user_teams(&self, user_id: String) -> Result<Vec<Team>, DaoError> {
+        println!("Listing user teams user_id={}", user_id);
+        let query = "SELECT ->member->team.* FROM $user_id;";
+        let response: Option<serde_json::Value> = self
+            .client
+            .query(query)
+            .bind(("user_id", RecordId::from((USER_TAG, &user_id))))
+            .await?
+            .take(0)?;
+
+        dbg!("{:?}", response)
+
+        todo!()
+
+        // let teams: Vec<TeamContent> = response
+        //             .get("->member")
+        //             .and_then(|member| member.get("->team"))
+        //             .and_then(|teams| teams.as_array())
+        //             .unwrap_or(&vec![])
+        //             .iter()
+        //             .filter_map(|team| serde_json::from_value::<TeamContent>(team.clone()).ok())
+        //             .collect::<Vec<TeamContent>>()
+
+        // println!("Parsing response");
+
+        // let teams: Vec<TeamContent> = response
+        //     .into_iter()
+        //     .filter_map(|entry| entry.get("->member")?.get("->team")?.as_array().cloned())
+        //     .flatten()
+        //     .map(|value| {
+        //         println!("Parsing {}", value);
+        //         serde_json::from_value(value)
+        //     })
+        //     .collect::<std::result::Result<Vec<TeamContent>, _>>()
+        //     .map_err(|err| {
+        //         // TODO Log
+        //         DaoError::InternalServerError("Failed to deserialize results".to_string())
+        //     })?;
+
+        // Ok(teams.into_iter().map(|team| Team {
+        //     id: team.id.id().to_string(),
+        //     name: team.name,
+        // }).collect())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use aws_sdk_dynamodb::{Client, Error};
-use aws_sdk_dynamodb::types::AttributeValue;
-use poem::{error::InternalServerError, EndpointExt, Result, Route, Server, listener::TcpListener, web::Data};
+use dao::Dao;
+use poem::{
+    EndpointExt, Result, Route, Server, error::InternalServerError, listener::TcpListener,
+    web::Data,
+};
 use poem_openapi::{
-    Object, OpenApi, OpenApiService, ApiResponse,
+    ApiResponse, Object, OpenApi, OpenApiService,
     param::Path,
     payload::{Json, PlainText},
 };
+use surrealdb::{engine::remote::ws::{Client, Ws}, opt::auth::Root, Surreal};
 use uuid::Uuid;
 
 mod dao;
@@ -29,6 +32,16 @@ struct Team {
     members: Vec<User>,
 }
 
+impl From<dao::Team> for Team {
+    fn from(value: dao::Team) -> Self {
+        Team {
+            id: value.id,
+            name: value.name,
+            members: vec![],
+        }
+    }
+}
+
 #[derive(Object)]
 struct CreateTeamInput {
     name: String,
@@ -48,29 +61,21 @@ impl Api {
     #[oai(path = "/teams", method = "post")]
     async fn create_team(
         &self,
-        Data(client): Data<&Client>,
+        Data(dao): Data<&Dao>,
         input: Json<CreateTeamInput>,
     ) -> Result<Json<Team>> {
-        let team = Team {
-            id: Uuid::new_v4().to_string(),
-            name: input.name.clone(),
-            members: vec![],
-        };
+        let team = dao.create_team(
+            input.name.clone(),
+            "someuser".into(),
+        ).await.map_err(InternalServerError)?;
 
-        let request = client
-            .put_item()
-            .table_name(TABLE_NAME)
-            .item("PK", AttributeValue::S(format!("TEAM#{}", team.id)))
-            .item("id", AttributeValue::S(team.id.clone()))
-            .item("name", AttributeValue::S(team.name.clone()));
-
-        request.send().await.map_err(InternalServerError)?;
-
-        Ok(Json(team))
+        Ok(Json(team.into()))
     }
 
     #[oai(path = "/teams", method = "get")]
-    async fn list_teams(&self, pool: Data<&Client>) -> Result<Json<Vec<Team>>> {
+    async fn list_teams(&self, Data(dao): Data<&Dao>) -> Result<Json<Vec<Team>>> {
+        dao.list_user_teams("someuser".to_string()).await.map_err(InternalServerError)?;
+
         let teams = vec![Team {
             id: Uuid::new_v4().to_string(),
             name: "Some name".to_string(),
@@ -83,21 +88,49 @@ impl Api {
     #[oai(path = "/teams/:id", method = "get")]
     async fn get_team(
         &self,
-        pool: Data<&Client>,
+        pool: Data<&Dao>,
         Path(id): Path<String>,
     ) -> Result<GetTeamResponse> {
-        Ok(
-            GetTeamResponse::Team(
-                Json(
-                    Team {
-                        id,
-                        name: "Some name".to_string(),
-                        members: vec![],
-                    }
-                )
-            )
-        )
+        Ok(GetTeamResponse::Team(Json(Team {
+            id,
+            name: "Some name".to_string(),
+            members: vec![],
+        })))
     }
+}
+
+async fn create_dao() -> Result<Dao, surrealdb::Error> {
+    let db: Surreal<Client> = Surreal::init();
+
+    db.connect::<Ws>("localhost:8000").await?;
+    db.signin(Root {
+        username: "root",
+        password: "root",
+    })
+    .await?;
+    db.use_ns("test").use_db("test").await?;
+
+    db.query("CREATE user:someuser").await?;
+//     db.query(
+//         "
+// DEFINE TABLE IF NOT EXISTS user SCHEMALESS
+//     PERMISSIONS FOR
+//         CREATE, SELECT WHERE $auth,
+//         FOR UPDATE, DELETE WHERE created_by = $auth;
+// DEFINE FIELD IF NOT EXISTS name ON TABLE person TYPE string;
+// DEFINE FIELD IF NOT EXISTS created_by ON TABLE person VALUE $auth READONLY;
+// 
+// DEFINE INDEX IF NOT EXISTS unique_name ON TABLE user FIELDS name UNIQUE;
+// DEFINE ACCESS IF NOT EXISTS account ON DATABASE TYPE RECORD
+// SIGNUP ( CREATE user SET name = $name, pass = crypto::argon2::generate($pass) )
+// SIGNIN ( SELECT * FROM user WHERE name = $name AND crypto::argon2::compare(pass, $pass) )
+// DURATION FOR TOKEN 15m, FOR SESSION 12h
+// ;",
+//     )
+//    .await?;
+
+    let dao = Dao::create(db);
+    return Ok(dao);
 }
 
 #[tokio::main]
@@ -108,13 +141,12 @@ async fn main() {
         OpenApiService::new(Api, "Hello World", "1.0").server("http://localhost:3000");
     let ui = api_service.swagger_ui();
 
-    let config = aws_config::load_from_env().await;
-    let client = aws_sdk_dynamodb::Client::new(&config);
+    let dao = create_dao().await.unwrap();
 
     let app = Route::new()
         .nest("/", api_service)
         .nest("/docs", ui)
-        .data(client);
+        .data(dao);
 
     Server::new(TcpListener::bind("127.0.0.1:3000"))
         .run(app)

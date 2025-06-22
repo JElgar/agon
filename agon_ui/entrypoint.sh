4399464f1a6165ea54e18ee79e52faabf8a035a3
James Elgar <jnelgar@amazon.com>
2025-06-22 23:09:51 +0100

    Add custom entrypoint for nginx container

--- agon_ui/entrypoint.sh
+++ agon_ui/entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+echo "Starting entrypoint script"
+echo "VITE_SUPABASE_URL ${VITE_SUPABASE_URL}"
+
 # Find the file where environment variables need to be replaced.
 runtimeEnvFile=$(ls -t /usr/share/nginx/html/assets/runtime-env*.js | head -n1)
 
@@ -9,3 +12,5 @@ runtimeEnvFile=$(ls -t /usr/share/nginx/html/assets/runtime-env*.js | head -n1)
 envsubst < "$runtimeEnvFile" > ./runtime-env_temp
 cp ./runtime-env_temp "$runtimeEnvFile"
 rm ./runtime-env_temp
+
+nginx -g "daemon off;"
